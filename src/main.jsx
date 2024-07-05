5ac8e4a0f90995533deed2e65cccb7ffc8e6ab2d
David Saldubehere <davesaldub@gmail.com>
2024-07-05 17:40:34 -0400

    added error page

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./routes/root.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
+import ErrorPage from "./error-page";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
